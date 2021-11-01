217097d5de41bceac89de48785cc688f0188e86d
JimR8 <jestrok32@protonmail.com>
2021-11-02 04:42:29 +1100

    update sliders

--- EvmosOdds.sol
+++ EvmosOdds.sol
@@ -2,12 +2,18 @@ pragma solidity ^0.4.18;
 
 contract EvmosOdds{
 
-    address public collector = 0xEFa23c86891Dc7eeE29BB68Feb6358B1A9Fec5c9; //Evmos testnet
+    address public collector = 0x7Fb4B2b6540C5A9e3c1b81BFBC537816731DF8b8; //Evmos testnet
     uint8 public playerSpin;
     uint256 betAmount;
     uint256 winAmount;
     uint256 public houseBalance;
     uint80 constant None = uint80(0);
+    string public lastresult;
+    uint256 public lastWinAmount;
+    uint256 public lastPlayerSpin;
+    uint256 public lastOdds;
+    uint256 public maxBet = 10000000000000000;
+
 
     //Generate random number between 1 and 100
     function Spin(address player) internal view returns (uint8) {
@@ -18,41 +24,48 @@ contract EvmosOdds{
     }
 
     function play(uint8 house) public payable {
-        if(msg.sender == collector){
-			  houseBalance += msg.value;
-		}else{
-        winAmount = (95 / house) * msg.value;
-    		if(winAmount*2 > houseBalance){
-    			msg.sender.transfer(msg.value);
-          clean();
-    		}else{
+        winAmount = (95 * msg.value) / house;
+        lastWinAmount = winAmount;
+        lastOdds = house;
+
+    	if(winAmount*2 > houseBalance || msg.value > maxBet){
+    		msg.sender.transfer(msg.value);
+            clean();
+    	}else{
           betAmount = msg.value;
           playerSpin = Spin(msg.sender);
     			checkAndFundWinner(house);
           clean();
-    		}
 		}
 
 	}
 
     //Reset fields
-    function clean() public{
+    function clean() private{
         betAmount = None;
         winAmount = None;
         playerSpin = 0;
     }
 
     //Check if winner
-    function checkAndFundWinner(uint8 house) public{
+    function checkAndFundWinner(uint8 house) private{
         while(playerSpin == house){
             Spin(msg.sender);
         }
+        lastPlayerSpin = playerSpin;
         if (playerSpin < house){
             msg.sender.transfer(winAmount);
-				    houseBalance -= winAmount;
+			houseBalance -= (winAmount - msg.value);
+		    lastresult = "Player Wins";
         }else{
-				    houseBalance += betAmount;
-			  }
+		    houseBalance += betAmount;
+		    lastresult = "House Wins";
+	    }
+    }
+
+
+    function() public payable{
+         houseBalance += msg.value;
     }
 
 	//Withdraw funds
@@ -60,6 +73,12 @@ contract EvmosOdds{
         if(msg.sender == collector){
 			     msg.sender.transfer(houseBalance);
 			     houseBalance =0;
-		    }
+		}
+    }
+
+    function changeMaxBet(uint256 newMaxBet) public payable{
+        if(msg.sender == collector){
+			     maxBet = newMaxBet;
+		}
     }
 }
